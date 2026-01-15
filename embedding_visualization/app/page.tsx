8c32f813e9712bfe8ad23ec4efdb676681e8b28b
Giacomo-De-Luca <giacomo.deluca@hotmail.it>
2026-01-15 03:29:08 +0100

    simplifying ui, avoid redrawing traces

--- embedding_visualization/app/page.tsx
+++ embedding_visualization/app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
       )}
       <SidebarInset className=" relative ">
         <div className="absolute top-0 left-0 right-0 z-50 p-2 pointer-events-none">
-          <div className="pointer-events-auto shadow-sm rounded-lg backdrop-blur-md border">
+          <div className="pointer-events-auto  rounded-lg ">
             <AppHeader
               collections={collections}
               collectionsLoading={collectionsLoading}
